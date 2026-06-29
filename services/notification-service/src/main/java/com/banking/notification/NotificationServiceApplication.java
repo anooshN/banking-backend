5ef99b7db4b6d54ffa1987752af21446c410ef00
anooshN <66909777+anooshN@users.noreply.github.com>
2026-06-29 07:39:04 -0400

    fix: services/notification-service/src/main/java/com/banking/notification/NotificationServiceApplication.java

--- services/notification-service/src/main/java/com/banking/notification/NotificationServiceApplication.java
+++ services/notification-service/src/main/java/com/banking/notification/NotificationServiceApplication.java
@@ -3,13 +3,19 @@ package com.banking.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAsync
-@EnableMongoAuditing
+@ComponentScan(basePackages = {
+    "com.banking.notification",
+    "com.banking.security",
+    "com.banking.common",
+    "com.banking.kafka",
+    "com.banking.exception"
+})
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
